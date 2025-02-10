7934374e9317a99cae0191b4f67d19224e350358
xiao <xiamoxo@gmail.com>
2025-02-09 17:53:53 -0800

    svg onMount

--- src/lib/Timemap.svelte
+++ src/lib/Timemap.svelte
@@ -1,9 +1,11 @@
 <script>
   import * as d3 from "d3";
-    let count = $state(0)
-    const increment = () => {
-        count += 1
-    }
+  import {onMount} from "svelte";
+
+  let count = $state(0)
+  const increment = () => {
+      count += 1
+  }
 
   const margin = {top: 80, right: 25, bottom: 30, left: 40},
   width = 450 - margin.left - margin.right,
@@ -11,47 +13,48 @@
 
   const rowHeight = 10;
 
-  let svg = d3.select(".timemapDia")
-    .append("svg")
-      .attr("width", width + margin.left + margin.right)
-      .attr("height", height + margin.top + margin.bottom)
-    .append("g")
-      .attr("transform",
-          "translate(" + margin.left + "," + margin.top + ")")
-    // .attr('fill','black')
-  ;
+  onMount(() =>{
+    let svg = d3.select(".timemapDia")
+      .append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+      .append("g")
+        .attr("transform",
+            "translate(" + margin.left + "," + margin.top + ")")
+      // .attr('fill','black')
+    ;
 
-  var color = d3.scaleLinear()
-  .range(["white", "#69b3a2"])
-  .domain([1,100])
+    d3.csv("/data/GROUP_GL.csv")
+      .then(d => {
+        let chgData = d.filter(data => 
+        data.IntervalStart != "" && data.IntervalStart != null )
+        // console.log(chgData)
 
-  d3.csv("/data/GROUP_GL.csv")
-    .then(d => {
-      let chgData = d.filter(data => 
-      data.IntervalStart != "" && data.IntervalStart != null )
-      // console.log(chgData)
+        let xScale = d3.scaleLinear()
+            .domain([d3.min(chgData, d => +d.IntervalStart),d3.max(chgData, d => +d.IntervalEnd)])
+            .range([0,width]);  
+            
+        let yScale = d3.scaleLinear()
+            .domain([d3.min(chgData, d => +d.GroupID),d3.max(chgData, d => +d.GroupID)])
+            .range([0,height]);
 
-      let xScale = d3.scaleLinear()
-          .domain([d3.min(chgData, d => d.IntervalStart),d3.max(chgData, d => d.IntervalEnd)])
-          .range([0,width]);  
-          
-      let yScale = d3.scaleLinear()
-          .domain([d3.min(chgData, d => d.GroupID),d3.max(chgData, d => d.GroupID)])
-          .range([0,height]);
+        // console.log(chgData.map(d => d.GroupID))
 
-      // console.log(chgData.map(d => d.GroupID))
+        let color = d3.scaleLinear()
+        .range(["black", "#69b3a2"])
+        .domain([d3.min(chgData, d => +d.ChgRate),d3.max(chgData, d => +d.ChgRate)])
 
-      svg.selectAll('rect')
-        .data(chgData)
-        .enter()
-        .append('rect')
-        .attr('x',xScale(chgData.IntervalStart))
-        .attr('y',yScale(chgData.GroupID))
-        .attr('height',rowHeight)
-        .attr('width',chgData.IntervalEnd - chgData.IntervalStart)
-        .attr('fill',color(chgData.ChgRate))
-    })
-
+        svg.selectAll('rect')
+          .data(chgData)
+          .enter()
+          .append('rect')
+          .attr('x',d => xScale(d.IntervalStart))
+          .attr('y',d => yScale(d.GroupID))
+          .attr('height',rowHeight)
+          .attr('width',d => (d.IntervalEnd - d.IntervalStart))
+          .attr('fill', d => color(d.ChgRate))
+      })
+  })
     
 </script>
 
